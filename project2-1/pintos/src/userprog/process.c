b78e9b4fd470b97d89955cbbcf52aa4abd490497
simey <simey1128@unist.ac.kr>
2022-04-02 08:45:41 +0900

    feat) 새로 생긴 thread에 이름만 전달하기 완료

--- project2-1/pintos/src/userprog/process.c
+++ project2-1/pintos/src/userprog/process.c
@@ -26,18 +26,22 @@ static bool load (const char *cmdline, void (**eip) (void), void **esp);
    before process_execute() returns.  Returns the new process's
    thread id, or TID_ERROR if the thread cannot be created. */
 tid_t
-process_execute (const char *file_name) 
+process_execute (const char *cmd) 
 {
   char *fn_copy;
   tid_t tid;
+
 
   /* Make a copy of FILE_NAME.
      Otherwise there's a race between the caller and load(). */
   fn_copy = palloc_get_page (0);
   if (fn_copy == NULL)
     return TID_ERROR;
-  strlcpy (fn_copy, file_name, PGSIZE);
+  strlcpy (fn_copy, cmd, PGSIZE);
 
+  char *file_name, *save_ptr;
+  file_name = strtok_r(cmd, " ", &save_ptr);
+  
   /* Create a new thread to execute FILE_NAME. */
   tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
   if (tid == TID_ERROR)
