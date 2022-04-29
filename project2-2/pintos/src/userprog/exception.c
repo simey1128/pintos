28d4bf9d8f71d3e08698215a7afb1f5653633652
simey <simey1128@unist.ac.kr>
2022-04-29 17:38:22 +0900

    feat) bad-@@ 테스트 통과 완료

--- project2-2/pintos/src/userprog/exception.c
+++ project2-2/pintos/src/userprog/exception.c
@@ -149,7 +149,7 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
-  if(!user || is_kernel_vaddr(fault_addr)) exit(-1);
+  if(fault_addr==NULL || !user || is_kernel_vaddr(fault_addr)) exit(-1);
 
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
