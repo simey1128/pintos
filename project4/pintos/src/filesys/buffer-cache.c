4216cd46630806adce773d4a532eb53f8ee579dc
simey <simey1128@unist.ac.kr>
2022-06-11 00:22:31 +0900

    feat) buffer-cache 필수 기능 구현

--- project4/pintos/src/filesys/buffer-cache.c
+++ project4/pintos/src/filesys/buffer-cache.c
@@ -2,39 +2,109 @@
 #include "filesys/filesys.h"
 
 void init_buffer_cache(){
-    buffer_cache = malloc(BLOCK_SECTOR_SIZE * BUFFER_LIST_SIZE);
-
     int i;
     for(i=0; i<BUFFER_LIST_SIZE; i++){
         buffer_list[i] = NULL;
+        buffer_list[i]->cache = (uint8_t*)malloc(sizeof(uint8_t*)*BLOCK_SECTOR_SIZE);
+        lock_init(&buffer_list[i]->lock);
     }
 }
 /*
-buffer_list에서 idx위치에 있는 buffer_entry 반환
-* invalid idx는 idx<0 or buffer_list[idx]가 비어있을 때
-* idx가 BUFFER_LIST_SIZE 이상이면, select victim, replace entry
-* 그렇지 않으면 buffer_list[idx] 반환
+buffer_list에서 sector위치에 있는 buffer_entry 반환
+* invalid sector는 sector<0 or buffer_list[sector]가 비어있을 때
+* sector가 BUFFER_LIST_SIZE 이상이면, select victim, replace entry
+* 그렇지 않으면 buffer_list[sector] 반환
 */
-struct buffer_entry* get_buffer_entry(block_sector_t idx){
-    if(idx<0 || buffer_list[idx]==NULL) return NULL;
+struct buffer_entry* get_buffer_entry(block_sector_t sector){
     
-    // buffer cache is full #TODO
-    if(idx>=BUFFER_LIST_SIZE){
-
-        return buffer_list[idx];
+    // buffer_list에 존재하는지 확인
+    int i;
+    for(i=0; i<BUFFER_LIST_SIZE; i++){
+        struct buffer_entry* be = buffer_list[i];
+        if(be->sector == sector) return be;
     }
-    return buffer_list[idx];
+
+    // buffer_list에 없으며, entry를 새로 만들어야함
+    for(i=0; i<BUFFER_LIST_SIZE; i++){
+        if(buffer_list[i] == NULL){
+            struct buffer_entry* be = buffer_list[i];
+            set_buffer_entry(be, sector, i);
+
+            return be;
+        }
+    }
+
+    // buffer_list가 꽉차서 entry를 replace해야함
+    return replace_buffer_entry(sector);    
 }
 
-int buffer_read(block_sector_t idx, void* _buffer, off_t bytes_read, off_t size, off_t offset){
+void buffer_read(block_sector_t idx, void* _buffer, off_t bytes_read, off_t size, off_t offset){
     struct buffer_entry* be = get_buffer_entry(idx);
+    lock_acquire(&be->lock);
 
-    // block_sector_t sector_idx = byte_to_sector (be->inode, offset);
-    // int sector_ofs = offset % BLOCK_SECTOR_SIZE;
+    block_read(fs_device, idx, be->cache);
+    memcpy(_buffer+bytes_read, be->cache + offset, size);
 
-    // block_read(fs_device, sector_idx, buffer_cache+(BLOCK_SECTOR_SIZE*idx));
-    // memcpy(_buffer+bytes_read, buffer_cache+(BLOCK_SECTOR_SIZE*idx)+sector_ofs, size);
+    be->last_clock = timer_ticks();
+    lock_release(&be->lock);
+}
 
-    //#TODO clock bit 설정하기
+void buffer_write(block_sector_t idx, void* buffer_, off_t bytes_written, off_t size, off_t offset){
+    struct buffer_entry* be = get_buffer_entry(idx);
+    lock_acquire(&be->lock);
 
+    block_write(fs_device, idx, be->cache);
+    memcpy(be->cache+offset , buffer_ + bytes_written, size);
+
+    be->last_clock = timer_ticks();
+    be->dirty = 1;
+    lock_release(&be->lock);
+}
+
+struct buffer_entry* replace_buffer_entry(block_sector_t sector){
+    int i, victim_idx;
+    struct buffer_entry* victim_be = NULL;
+
+    for(i=0; i<BUFFER_LIST_SIZE; i++){
+        struct buffer_entry* target_be = buffer_list[i];
+
+        if(victim_be == NULL || target_be->last_clock < victim_be->last_clock){
+            victim_be = target_be;
+            victim_idx = i;
+        }
+    }
+
+    lock_acquire(&victim_be->lock);
+    if(victim_be->dirty) flush_buffer_entry(victim_be);
+    set_buffer_entry(victim_be, sector, victim_idx);
+
+    lock_release(&victim_be->lock);
+    return victim_be;
+}
+
+void flush_buffer_entry(struct buffer_entry* be){
+    block_write(fs_device, be->sector, be->cache);
+    be->dirty = 0;
+    be->last_clock = timer_ticks();
+}
+
+void set_buffer_entry(struct buffer_entry* be, block_sector_t sector, int idx){
+    be->inode = inode_open(sector);
+    be->sector = sector;
+    be->dirty = 0;
+    be->last_clock = timer_ticks();
+}
+
+void free_buffer(){
+    int i;
+    for(i=0; i<BUFFER_LIST_SIZE; i++){
+        struct buffer_entry* be = buffer_list[i];
+        lock_acquire(&be->lock);
+        if(be->dirty){
+            flush_buffer_entry(be);
+        }
+        free(be->cache);
+        lock_release(&be->lock);
+        buffer_list[i] = NULL;
+    }
 }
--- project4/pintos/src/filesys/buffer-cache.h
+++ project4/pintos/src/filesys/buffer-cache.h
@@ -1,5 +1,6 @@
 #include "filesys/inode.h"
 #include "list.h"
+#include "threads/synch.h"
 #define BUFFER_LIST_SIZE 64
 
 struct buffer_entry{
@@ -7,15 +8,26 @@ struct buffer_entry{
     struct inode* inode; // in-memory inode
     block_sector_t sector; //disk block for FCB
 
-    void* file_data;
+    uint8_t* cache;
 
     int dirty; //0-not dirty, 1-dirty -> need write behind
     uint32_t last_clock;
-    //# TODO - lock?
+    struct lock lock;
 };
 
 struct buffer_entry* buffer_list[BUFFER_LIST_SIZE];
-uint8_t* buffer_cache;
+
+
+
 void init_buffer_cache();
+void set_buffer_entry(struct buffer_entry* be, block_sector_t sector, int idx);
+void flush_buffer_entry(struct buffer_entry* be);
 struct buffer_entry* get_buffer_entry(block_sector_t idx);
-int buffer_read(block_sector_t idx, void* buffer_, off_t bytes_read, off_t size, off_t offset);
+
+
+//lock 사용됨
+void buffer_read(block_sector_t idx, void* buffer_, off_t bytes_read, off_t size, off_t offset);
+void buffer_write(block_sector_t idx, void* buffer_, off_t bytes_written, off_t size, off_t offset);
+void free_buffer(void);
+struct buffer_entry* replace_buffer_entry(block_sector_t sector);
+
